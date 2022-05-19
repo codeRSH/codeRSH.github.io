adab7a2a56962c9bfcc527cd9b0e8dd47eb6af59
codersh <mail.ravs@yahoo.co.in>
2022-05-19 10:10:55 +0530

    20220519

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,13 @@
+var store = [{
+        "title": "First post copy",
+        "excerpt":"First Post Today I want to take another plunge into maintaining my own site and blog. I realize currently it’s really barebone. Partly it’s deliberate to keep it clean so that focus can be only on content. But I will definetely improve it as time goes by, and hope to...","categories": [],
+        "tags": [],
+        "url": "/blog/first",
+        "teaser": null
+      },{
+        "title": "First post",
+        "excerpt":"First Post Today I want to take another plunge into maintaining my own site and blog. I realize currently it’s really barebone. Partly it’s deliberate to keep it clean so that focus can be only on content. But I will definetely improve it as time goes by, and hope to...","categories": [],
+        "tags": [],
+        "url": "/blog/first",
+        "teaser": null
+      }]
